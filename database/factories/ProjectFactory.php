ce7ac7a41423fade25136e767ddb3e92f79e6b71
abdulrahmanRadan <aboda123li123@gmail.com>
2024-11-30 17:15:51 +0300

    Generate various types of fake data for project and category factories

--- database/factories/ProjectFactory.php
+++ database/factories/ProjectFactory.php
@@ -2,6 +2,7 @@
 
 namespace Database\Factories;
 use App\Models\Project;
+use App\Models\Category; 
 
 use Illuminate\Database\Eloquent\Factories\Factory;
 
@@ -18,9 +19,9 @@ class ProjectFactory extends Factory
     public function definition(): array
     {
         return [
-            'title'=> fake()->sentence(),
+            'title'=> fake()->catchPhrase(),
             'description'=> fake()->text(),
-            'category_id' => '1',
+            'category_id' => Category::factory(),
             'status' =>fake()->randomElement(['completed', 'in_progress', 'on_hold'])
         ];
     }
