d8327c01a3240c0cef996f21fa2ebb7935f010e6
abdulrahmanRadan <aboda123li123@gmail.com>
2024-12-02 17:30:25 +0300

    create job table model and -mf

--- database/migrations/2024_12_02_133614_create_tags_table.php
+++ database/migrations/2024_12_02_133614_create_tags_table.php
@@ -13,7 +13,13 @@ return new class extends Migration
     {
         Schema::create('tags', function (Blueprint $table) {
             $table->id();
+            $table->string('name');
             $table->timestamps();
+        });
+        Schema::create('job_tag', function (Blueprint $table) {
+            $table->id();
+            $table->foreignIdFor(\App\Models\Job::class, 'job_listing_id')->constrined()->cascadeOnDelete();
+            $table->foreignIdFor(\App\Models\Tag::class)->constrined()->cascadeOnDelete();
         });
     }
 
@@ -23,5 +29,6 @@ return new class extends Migration
     public function down(): void
     {
         Schema::dropIfExists('tags');
+        Schema::dropIfExists('job_tag');
     }
 };
