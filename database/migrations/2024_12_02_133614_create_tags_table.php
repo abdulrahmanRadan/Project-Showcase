6b70c086ca028fc283df67beace6e96ba64b077e
abodradan <abdulrahmanraadan@gmail.com>
2024-12-07 22:29:32 +0300

    fix the probelm in job_tag table

--- database/migrations/2024_12_02_133614_create_tags_table.php
+++ database/migrations/2024_12_02_133614_create_tags_table.php
@@ -4,6 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
+
 return new class extends Migration
 {
     /**
@@ -18,8 +19,9 @@ return new class extends Migration
         });
         Schema::create('job_tag', function (Blueprint $table) {
             $table->id();
-            $table->foreignIdFor(\App\Models\Job::class, 'job_listing_id')->constrined()->cascadeOnDelete();
-            $table->foreignIdFor(\App\Models\Tag::class)->constrined()->cascadeOnDelete();
+            $table->foreignIdFor(App\Models\Job::class, 'job_listing_id')->constrained()->cascadeOnDelete();
+            $table->foreignIdFor(App\Models\Tag::class)->constrained()->cascadeOnDelete();
+            $table->timestamps();
         });
     }
 
